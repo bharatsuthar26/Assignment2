127c583ba10b4419bbcd1ee1f84dc8861316c459
bharatsuthar26 <sutharbharat921@gmail.com>
2022-11-27 20:33:44 +0530

    add form details in rows

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require("express");
 const app = express();
 const fs= require('fs');
 app.use(express.json());
+const path = require('path')
+app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended:false}));
-const port = 3500;
+const port = 5000;
 
 app.set("view engine", "pug");
 app.set("views", "./views");
@@ -26,7 +28,7 @@ app.get("/aboutus", (req, res) => {
   res.render("aboutus");
 });
 app.get("/details", (req, res) => {
-  res.sendFile('contact.txt',{root:'./'});
+  res.render("details");
 });
 
 app.get("/", (req, res) => {
@@ -39,14 +41,33 @@ app.post("/postdata",(req,res)=>{
   let email = req.body.email;
   let subject= req.body.subject;
   let comments= req.body.comments
-  const data = {name:req.body.name,email:req.body.email , subject:req.body.subject,comments:comments}
-  fs.appendFile('contact.txt', `${JSON.stringify(data)}\n` , function (err) {
-    if (err) throw err;
-    console.log('Saved!');
-    res.send(`Form Submitted Successfully`)
+  // const data = {name:req.body.name,email:req.body.email , subject:req.body.subject,comments:comments}
+  // fs.appendFile('contact.txt', `${JSON.stringify(data)}\n` , function (err) {
+  //   if (err) throw err;
+  //   console.log('Saved!');
+  //   res.send(`Form Submitted Successfully`)
   
-  });
+  // });
+  
+  let data=`
+  <tr>
+      <td>${name}</td>
+      <td>${email}</td>
+      <td>${subject}</td>
+      <td>${comments}</td>
+  <tr>
+      `
+      if(fs.existsSync(`./user`)){
+          fs.appendFileSync(`./user/detail.pug`,data)
+          res.redirect("/contact")
+      }
+      else{
+          fs.mkdirSync(`./user/`);
+          fs.writeFileSync(`./user/detail.pug`,`${data.toString()}`);
+          res.send("CREATE FILE OF CONTACT")
+      }
 })
+
 
 app.listen(port, (err) => {
   if (err) {
